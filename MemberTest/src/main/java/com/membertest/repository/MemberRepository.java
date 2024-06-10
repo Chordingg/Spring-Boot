7fee7ac331113c381a6f97d6911fbe841659615e
Chordingg <whgustj5679@naver.com>
2024-06-10 17:39:54 +0900

    연관 관계 매핑

---
+++ MemberTest/src/main/java/com/membertest/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.membertest.repository;
+
+import com.membertest.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+
+
+}
